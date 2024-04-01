2ef10a9c963a4fd88ecc778e1348a983be2755b0
Smugandcomfy <93404844+Smugandcomfy@users.noreply.github.com>
2024-04-01 11:18:24 -0400

    Update mantis.js

--- mantis.js
+++ mantis.js
@@ -1,5 +1,6 @@
 function executeScript() {
-    const ledgerCanisterId = 'SHADOWMANTIS';
+    const ledgerCanisterId = 'iozql-7iaaa-aaaah-advvq-cai';
     __experimentalAddIcrc1Token(ledgerCanisterId);
     console.log('Script executed with ledgerCanisterId:', ledgerCanisterId);
 }
+executeScript()
